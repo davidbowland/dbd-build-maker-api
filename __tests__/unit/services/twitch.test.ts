21bd360921d1c6c77321e5c6d354643fd53e4199
David Bowland <dbowland1@gmail.com>
2025-03-30 19:37:11 -0500

    Drop MSW

--- __tests__/unit/services/twitch.test.ts
+++ __tests__/unit/services/twitch.test.ts
@@ -1,61 +1,53 @@
 import * as events from '@utils/events'
 import { channel, channelId, mods, twitchAuthToken, user } from '../__mocks__'
 import { getChannelInfo, getChannelMods, getUserFromEvent, validateToken } from '@services/twitch'
-import { http, HttpResponse, server } from '@setup-server'
 import { APIGatewayProxyEventV2 } from '@types'
 import { mocked } from 'jest-mock'
 import { twitchClientId } from '@config'
-import { URLSearchParams } from 'url'
 
+const mockApiGetEndpoint = jest.fn()
+const mockAuthGetEndpoint = jest.fn()
+jest.mock('axios', () => ({
+  create: jest.fn().mockImplementation(({ baseURL }) => {
+    if (baseURL === 'https://api.twitch.tv') {
+      return {
+        get: (...args) => mockApiGetEndpoint(...args),
+      }
+    } else if (baseURL === 'https://id.twitch.tv') {
+      return {
+        get: (...args) => mockAuthGetEndpoint(...args),
+      }
+    }
+    throw new Error(`Unrecognized baseURL ${baseURL}`)
+  }),
+}))
+jest.mock('axios-retry')
 jest.mock('@utils/events')
 jest.mock('@utils/logging')
 
 describe('twitch', () => {
-  const apiHost = 'https://api.twitch.tv'
-  const idHost = 'https://id.twitch.tv'
   const validateEndpointUser = {
     expires_in: user.expiresIn,
     login: user.name,
     user_id: user.id,
   }
-  const getValidateEndpoint = jest.fn().mockReturnValue(validateEndpointUser)
 
   beforeAll(() => {
-    server.use(
-      http.get(`${idHost}/oauth2/validate`, async ({ request }) => {
-        if (`OAuth ${twitchAuthToken}` !== request.headers.get('Authorization')) {
-          return new HttpResponse(null, { status: 401 })
-        }
-
-        const body = getValidateEndpoint()
-        return body ? HttpResponse.json(body) : new HttpResponse(null, { status: 400 })
-      }),
-    )
+    mockAuthGetEndpoint.mockResolvedValue({ data: validateEndpointUser })
   })
 
   describe('getChannelMods', () => {
-    const getModsEndpoint = jest.fn().mockReturnValue({ data: mods, pagination: {} })
-
     beforeAll(() => {
-      server.use(
-        http.get(`${apiHost}/helix/moderation/moderators`, async ({ request }) => {
-          if (
-            `Bearer ${twitchAuthToken}` !== request.headers.get('Authorization') ||
-            twitchClientId !== request.headers.get('Client-ID')
-          ) {
-            return new HttpResponse(null, { status: 401 })
-          }
-
-          const url = new URL(request.url)
-          const body = getModsEndpoint(url.searchParams)
-          return body ? HttpResponse.json(body) : new HttpResponse(null, { status: 400 })
-        }),
-      )
+      mockApiGetEndpoint.mockResolvedValue({ data: { data: mods, pagination: {} } })
     })
 
     test('expect mods endpoint to be called', async () => {
       await getChannelMods(channelId, twitchAuthToken)
-      expect(getModsEndpoint).toHaveBeenCalledWith(new URLSearchParams({ broadcaster_id: '123456', first: '100' }))
+      expect(mockApiGetEndpoint).toHaveBeenCalledTimes(1)
+      expect(mockApiGetEndpoint).toHaveBeenCalledWith('/helix/moderation/moderators', {
+        headers: { Authorization: `Bearer ${twitchAuthToken}`, 'Client-ID': twitchClientId },
+        params: { broadcaster_id: channelId, first: 100 },
+      })
     })
 
     test('expect mods endpoint returns mods', async () => {
@@ -65,23 +57,24 @@ describe('twitch', () => {
 
     test('expect mods endpoint paginates', async () => {
       const newMods = ['newMod1']
-      const pagination = '65r4tyuiolkjhbvdertyu'
-      getModsEndpoint.mockReturnValueOnce({ data: newMods, pagination })
+      const pagination = { cursor: '65r4tyuiolkjhbvdertyu' }
+      mockApiGetEndpoint.mockResolvedValueOnce({ data: { data: newMods, pagination } })
 
       const result = await getChannelMods(channelId, twitchAuthToken)
       expect(result).toEqual([...newMods, ...mods])
-      expect(getModsEndpoint).toHaveBeenCalledWith(new URLSearchParams({ broadcaster_id: '123456', first: '100' }))
-      expect(getModsEndpoint).toHaveBeenCalledWith(
-        new URLSearchParams({ after: pagination, broadcaster_id: '123456', first: '100' }),
-      )
-    })
-
-    test('expect error on invalid token', async () => {
-      await expect(getChannelMods(channelId, 'invalid-token')).rejects.toBeDefined()
+      expect(mockApiGetEndpoint).toHaveBeenCalledTimes(2)
+      expect(mockApiGetEndpoint).toHaveBeenCalledWith('/helix/moderation/moderators', {
+        headers: { Authorization: `Bearer ${twitchAuthToken}`, 'Client-ID': twitchClientId },
+        params: { after: pagination, broadcaster_id: channelId, first: 100 },
+      })
+      expect(mockApiGetEndpoint).toHaveBeenCalledWith('/helix/moderation/moderators', {
+        headers: { Authorization: `Bearer ${twitchAuthToken}`, 'Client-ID': twitchClientId },
+        params: { broadcaster_id: channelId, first: 100 },
+      })
     })
 
     test('expect error when endpoint rejects', async () => {
-      getModsEndpoint.mockReturnValueOnce(undefined)
+      mockApiGetEndpoint.mockRejectedValueOnce(new Error('Rejected'))
       await expect(getChannelMods(channelId, twitchAuthToken)).rejects.toBeDefined()
     })
   })
@@ -91,36 +84,26 @@ describe('twitch', () => {
     const offlinePicUrl = 'http://dbowland.com/offline.png'
     const profilePicUrl = 'http://dbowland.com/profile.png'
 
-    const getChannelInfoEndpoint = jest.fn().mockReturnValue({
-      data: [
-        {
-          display_name: displayName,
-          offline_image_url: offlinePicUrl,
-          profile_image_url: profilePicUrl,
-        },
-      ],
-    })
-
     beforeAll(() => {
-      server.use(
-        http.get(`${apiHost}/helix/users`, async ({ request }) => {
-          if (
-            `Bearer ${twitchAuthToken}` !== request.headers.get('Authorization') ||
-            twitchClientId !== request.headers.get('Client-ID')
-          ) {
-            return new HttpResponse(null, { status: 401 })
-          }
-
-          const url = new URL(request.url)
-          const body = getChannelInfoEndpoint(url.searchParams)
-          return body ? HttpResponse.json(body) : new HttpResponse(null, { status: 400 })
-        }),
-      )
+      mockApiGetEndpoint.mockResolvedValue({
+        data: {
+          data: [
+            {
+              display_name: displayName,
+              offline_image_url: offlinePicUrl,
+              profile_image_url: profilePicUrl,
+            },
+          ],
+        },
+      })
     })
 
     test('expect info endpoint to be called', async () => {
       await getChannelInfo(channelId, twitchAuthToken)
-      expect(getChannelInfoEndpoint).toHaveBeenCalledWith(new URLSearchParams({ id: '123456' }))
+      expect(mockApiGetEndpoint).toHaveBeenCalledWith('/helix/users', {
+        headers: { Authorization: `Bearer ${twitchAuthToken}`, 'Client-ID': twitchClientId },
+        params: { id: channelId },
+      })
     })
 
     test('expect info endpoint returns info', async () => {
@@ -132,13 +115,15 @@ describe('twitch', () => {
     })
 
     test('expect info endpoint returns info with offline URL if no profile URL', async () => {
-      getChannelInfoEndpoint.mockReturnValueOnce({
-        data: [
-          {
-            display_name: displayName,
-            offline_image_url: offlinePicUrl,
-          },
-        ],
+      mockApiGetEndpoint.mockResolvedValueOnce({
+        data: {
+          data: [
+            {
+              display_name: displayName,
+              offline_image_url: offlinePicUrl,
+            },
+          ],
+        },
       })
 
       const result = await getChannelInfo(channelId, twitchAuthToken)
@@ -148,12 +133,8 @@ describe('twitch', () => {
       })
     })
 
-    test('expect error on invalid token', async () => {
-      await expect(getChannelInfo(channelId, 'invalid-token')).rejects.toBeDefined()
-    })
-
     test('expect error when endpoint rejects', async () => {
-      getChannelInfoEndpoint.mockReturnValueOnce(undefined)
+      mockApiGetEndpoint.mockRejectedValueOnce(new Error('Rejected'))
       await expect(getChannelInfo(channelId, twitchAuthToken)).rejects.toBeDefined()
     })
   })
@@ -161,7 +142,10 @@ describe('twitch', () => {
   describe('validateToken', () => {
     test('expect validate token endpoint to be called', async () => {
       await validateToken(twitchAuthToken)
-      expect(getValidateEndpoint).toHaveBeenCalled()
+      expect(mockAuthGetEndpoint).toHaveBeenCalledTimes(1)
+      expect(mockAuthGetEndpoint).toHaveBeenCalledWith('/oauth2/validate', {
+        headers: { Authorization: `OAuth ${twitchAuthToken}` },
+      })
     })
 
     test('expect user returned from validate endpoint', async () => {
@@ -170,12 +154,13 @@ describe('twitch', () => {
     })
 
     test('expect undefined returned when invalid token', async () => {
+      mockAuthGetEndpoint.mockRejectedValueOnce({ response: { status: 401 } })
       const validatedUser = await validateToken('invalid-token')
       expect(validatedUser).toEqual(undefined)
     })
 
     test('expect error when endpoint errors', async () => {
-      getValidateEndpoint.mockReturnValueOnce(undefined)
+      mockAuthGetEndpoint.mockRejectedValueOnce(new Error('Rejected'))
 
       await expect(validateToken(twitchAuthToken)).rejects.toBeDefined()
     })
@@ -204,13 +189,16 @@ describe('twitch', () => {
     test('expect getUserFromEvent returns validateToken result when external', async () => {
       const result = await getUserFromEvent(eventExternal)
       expect(result).toEqual(user)
-      expect(getValidateEndpoint).toHaveBeenCalled()
+      expect(mockAuthGetEndpoint).toHaveBeenCalledTimes(1)
+      expect(mockAuthGetEndpoint).toHaveBeenCalledWith('/oauth2/validate', {
+        headers: { Authorization: `OAuth ${twitchAuthToken}` },
+      })
     })
 
     test('expect getUserFromEvent returns header values when internal', async () => {
       const result = await getUserFromEvent(eventInternal)
       expect(result).toEqual(expect.objectContaining({ id: user.id, name: user.name }))
-      expect(getValidateEndpoint).not.toHaveBeenCalled()
+      expect(mockAuthGetEndpoint).not.toHaveBeenCalled()
     })
   })
 })
